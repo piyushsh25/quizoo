d9765396d97496035a9a7ea5b95b4cdb548e6d47
piyush <piyushsharmashpr@gmail.com>
2022-08-21 21:51:22 +0530

    [router] added router to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { HomePage } from "./components/HomePage/HomePage";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <HomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
