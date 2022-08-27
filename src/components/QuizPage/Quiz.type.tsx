c084197930460d4d6ac6821c8c6dda99f14ba172
piyush <piyushsharmashpr@gmail.com>
2022-08-27 13:34:14 +0530

    [feat] change question on next

--- src/components/QuizPage/Quiz.type.tsx
+++ src/components/QuizPage/Quiz.type.tsx
@@ -3,3 +3,6 @@ import { QuizType } from "../../QuizContext/Database.type";
 export type QuizData = {
   state: QuizType;
 };
+export type StateType = {
+  questionIndex: number;
+};
--- src/components/QuizPage/QuizBody.tsx
+++ src/components/QuizPage/QuizBody.tsx
@@ -1,17 +1,35 @@
-import { QuizData } from "./Quiz.type";
+import { QuizData, StateType } from "./Quiz.type";
 import "./Quiz.css";
+import { useState } from "react";
 export const QuizBody = ({ state }: QuizData) => {
   const { questions } = state;
+  const [questionIndex, setQuestionIndex] =
+    useState<StateType["questionIndex"]>(0);
+  function nextQuestionHandler() {
+    setQuestionIndex((prev) => prev + 1);
+  }
   return (
     <div className="quiz-body-container">
-      <div className="question-container">{questions[0].question}</div>
+      <div className="question-container">
+        {questions[questionIndex].question}
+      </div>
       <div className="option-container">
-        {questions[0].options.map((option,index) => {
-          return <div className="option-individual">{index+1}. {option.option}</div>;
+        {questions[questionIndex].options.map((option, index) => {
+          return (
+            <div
+              className="option-individual"
+              key={option.option}
+              onClick={nextQuestionHandler}
+            >
+              {index + 1}. {option.option}
+            </div>
+          );
         })}
       </div>
       <div className="button-container">
-      <button className="next-button">Next {"-->"}</button>
+        <button className="next-button" onClick={nextQuestionHandler}>
+          Next {"-->"}
+        </button>
       </div>
     </div>
   );
